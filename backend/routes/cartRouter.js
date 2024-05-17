f5e26b45dc499359a3fac56fbe2ab2cbe0d430fd
Salmakhaled204 <salma2021.khaled@gmail.com>
2024-05-17 23:11:55 +0300

    Update cartRouter.js

--- backend/routes/cartRouter.js
+++ backend/routes/cartRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router();provided
 const cartController = require("../controllers/cartController");
 const authorizationMiddleware=require('../middleware/authorization')
 
